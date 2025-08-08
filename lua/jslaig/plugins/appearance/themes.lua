2028a9608e7b8a3959d11327ebb8b31dab8c4229
JSlaig <jsaligz@gmail.com>
2025-08-08 12:18:38 +0200

    Removed broken themes, activated the other one that looks neat

--- lua/jslaig/plugins/appearance/themes.lua
+++ lua/jslaig/plugins/appearance/themes.lua
@@ -1,8 +1,5 @@
 return {
 	{ "catppuccin/nvim", as = "catppuccin" },
-	-- { "olimorris/onedarkpro.nvim", as = "onedarkpro" },
-    -- { "scottmckendry/cyberdream.nvim", as = "cyberdream" },
     -- { "rose-pine/neovim", as = "rose-pine" },
-    -- { "AlexvZyl/nordic.nvim", as = "nordic" },
-    -- { "craftzdog/solarized-osaka.nvim", as = "solarized-osaka" }
+    { "AlexvZyl/nordic.nvim", as = "nordic" },
 }
