ec5920145089b69a73af237b66d28c162b24fbd6
JSlaig <jsaligz@gmail.com>
2025-08-10 23:58:12 +0200

    Substituted leap.nvim to flash.nvim for advanced features

---
+++ lua/jslaig/plugins/navigation/flash.lua
@@ -0,0 +1,20 @@
+return {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+        modes = {
+            search = {
+                enabled = true,
+            },
+            char = {
+                jump_labels = true,
+            },
+        },
+    },
+    
+    keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+}
