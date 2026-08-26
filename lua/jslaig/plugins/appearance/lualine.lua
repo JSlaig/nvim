4af026f1eaff7f3fe56e6c5bdabd5ace4658be3d
jsalinero <jsalinero@indra.es>
2026-08-26 08:33:45 +0200

    added buffer save status on lualine

--- lua/jslaig/plugins/appearance/lualine.lua
+++ lua/jslaig/plugins/appearance/lualine.lua
@@ -1,26 +1,64 @@
 return {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+
     config = function()
         vim.defer_fn(function()
+            local modified = {
+                function()
+                    return vim.bo.modified and "  UNSAVED  " or ""
+                end,
+
+                color = {
+                    fg = "#FFFFFF",
+                    bg = "#E06C75",
+                    gui = "bold",
+                },
+            }
             require("lualine").setup({
                 options = {
                     icons_enabled = true,
-                    theme = 'auto',
-                    component_separators = '❖',
-                    section_separators = { left = '', right = '' },
+                    theme = "auto",
+                    component_separators = "❖",
+                    section_separators = { left = "", right = "" },
                 },
+
                 sections = {
-                    lualine_a = { 'mode' },
+                    lualine_a = { "mode" },
+
                     lualine_b = {
-                        { 'filename', path = 1 },
+                        { "filename", path = 1 },
+                        modified,
                     },
-                    lualine_c = { 'filename' },
-                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                    lualine_y = { 'progress' },
-                    lualine_z = { function() return os.date() end }
-                }
+
+                    lualine_c = {},
+
+                    lualine_x = {
+                        "encoding",
+                        "fileformat",
+                        "filetype",
+                    },
+
+                    lualine_y = { "progress" },
+                    lualine_z = {
+                        function()
+                            return os.date()
+                        end,
+                    },
+                },
+
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {
+                        { "filename", path = 1 },
+                        modified,
+                    },
+                    lualine_c = {},
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = {},
+                },
             })
         end, 0)
     end,
